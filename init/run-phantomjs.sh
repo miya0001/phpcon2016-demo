d370cbe2c5fcc62aab7720967a9de90ac6fb11a2
miya0001 <miya@wpist.me>
2016-10-31 18:58:13 +0900

    some fix

--- init/run-phantomjs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-phantomjs \
-    --webdriver=4444 \
-    --ignore-ssl-errors=yes \
-    --cookies-file=/tmp/webdriver_cookie.txt \
-    > /tmp/webdriver_output.txt &
